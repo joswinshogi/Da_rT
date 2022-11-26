56bf690c1574193df9c27199fe8a440101b5c69a
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2022-11-26 13:34:28 +0530

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,25 @@
+void main(){
+  //integer data type used to store integers
+  int a=10;
+  //double data type is used for store decimal values
+  // o/p=20.0
+  double b=20;
+  print('$a $b');
+  //'num' is a datatype used for store any type of values(eg: int,float...)
+  num n1=10.0000;
+  num n2=2;
+  print('$n1\n$n2');
+  //int string data type can use both single quotes('') for single line sting
+  //double quotes ("")for double line string
+  //3 quotes for(''')multi line string
+  String nam='joswin';
+  String nam2="I'm joswin shogi";
+  print('$nam\n$nam2');
+  String add='''my name
+  my address
+  ''';
+  print('$add');
+  //for finding length of the string. It include white spaces toooo.
+  print(nam.length);
+
+}
